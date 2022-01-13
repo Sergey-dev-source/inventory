1ccc6a85b10b3d73a43232d6886a75800b82447a
Andranik Hoghmrtsyan <h.andranik62@gmail.com>
2022-01-13 19:04:47 +0400

    change

--- backend/app/Http/Controllers/MenuController.php
+++ backend/app/Http/Controllers/MenuController.php
@@ -8,7 +8,12 @@ use Illuminate\Http\Request;
 class MenuController extends Controller
 {
     public function index () {
-        $section = Section::with('category')->get();
+        $section = Section::with('category')->whereHas('category', function($q)
+        {
+
+            $q->where('active', '=', 1);
+
+        })->get();
         return response()->json($section);
     }
 }
